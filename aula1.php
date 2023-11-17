963b5c1a45850b37087b9325318b1a07b2973094
ygor aragao <ygor-nunes@uni9.edu.br>
2023-11-16 21:27:01 -0300

    padaria
    iniciando php

--- aula1.php
+++ aula1.php
@@ -12,3 +12,7 @@ echo "Meu nome $nome e $sobrenome moro em $cidade tenho $idade sou $estadocivil 
 e $estudante";
 
 ?>
+
+<!-- $ é as variaveis -->
+<!-- = significa " recebe" a informaçao que está entre aspas -->
+<!-- echo ele reproduz como a forma desejada -->
