6c25a0d321673553462a5f1ee3ce16d1ef7a4a55
Edvaldo Alcântara Oliveira <ygor-nunes@uni9.edu.br>
2023-11-16 21:22:16 -0300

    padaria dom bosco
    iniciando o php

---
+++ aula1.php
@@ -0,0 +1,14 @@
+<?php 
+
+$nome = "ygor aragao";
+$sobrenome = "cruz";
+$cidade = "São Paulo-Sp";
+$idade = "31 anos";
+$estadocivil = "casado";
+$profissao = "pintor";
+$estudante = "programador de web";
+
+echo "Meu nome $nome e $sobrenome moro em $cidade tenho $idade sou $estadocivil trabalho como $profissao 
+e $estudante";
+
+?>
